cc37bf023b1a6bd8f1002803ed33754a346a06e0
sonu kumar <dev.sonukumarsingh@gmail.com>
2024-02-28 17:05:19 +0530

    add imag3

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Navigation = () => {
   return (
     <div>
-      <img src="/public/1.jpeg" alt="branding image" srcset="" />
+      <img
+        src="https://i.pinimg.com/originals/e5/9c/82/e59c829455bd9cdc12930cb72c7253f3.png"
+        alt="branding image"
+        srcset=""
+      />
     </div>
   );
 };
