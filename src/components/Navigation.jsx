14470ab8ae9d88f0538bb3f433600670c42dd055
sonu kumar <dev.sonukumarsingh@gmail.com>
2024-02-28 17:20:54 +0530

    add imag4

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,11 +3,7 @@ import React from "react";
 const Navigation = () => {
   return (
     <div>
-      <img
-        src="https://i.pinimg.com/originals/e5/9c/82/e59c829455bd9cdc12930cb72c7253f3.png"
-        alt="branding image"
-        srcset=""
-      />
+      <img src="/public/logo.png" alt="branding image" srcset="" />
     </div>
   );
 };
