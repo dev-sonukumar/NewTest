4e0b4acfaa4502e3a28309211b2877af6c86ca26
sonu kumar <dev.sonukumarsingh@gmail.com>
2024-02-28 16:53:17 +0530

    update

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <img src="/public/1.png" alt="branding" srcset="" />
+    </div>
+  );
+};
+
+export default Navigation;
